1c62db75998ec0149eb16bfbff4f40d181d9d444
Caitlin Pierce <caitlinpierce@dcc.edu>
2020-05-01 16:55:23 -0500

    Start button counts seconds when clicked

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function displayTime() {
-    let date = new Date();
-    let time = date.toLocaleTimeString();
-    document.querySelector('.clock').textContent = time;
-  }
 
-  displayTime();
-  const createClock = setInterval(displayTime, 1000);
+//  A "Stop" button to pause/stop it.
+//   A "Reset" button to reset the time back to 0.
+//The time display to show the number of seconds elapsed, rather than the actual time.
+
+const secondsElement = document.querySelector('#seconds-elapsed')
+let seconds = 0
+
+// A "Start" button to start the stopwatch running.
+document.querySelector('#start').onclick = function() {
+    setInterval(function() {
+     seconds++ 
+     secondsElement.innerText = seconds
+    }, 1000);
+    
+}
